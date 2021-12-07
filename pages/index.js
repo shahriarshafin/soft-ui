afa7a7a6ba2380c590c43875b725d3f83aa9fb26
shafin <shafinshahriar340@gmail.com>
2021-12-07 23:43:51 +0600

    Features Section Added

--- pages/index.js
+++ pages/index.js
@@ -1,80 +1,17 @@
-import {
-	FaClock,
-	FaCode,
-	FaCodeBranch,
-	FaCube,
-	FaFolder,
-	FaJsSquare,
-	FaQrcode,
-	FaSeedling,
-} from 'react-icons/fa';
 import BootNav from '../components/BootNav';
-import FeatureCard from '../components/FeatureCard';
 import ProfileCard from '../components/profileCard';
-import SectionInfo from '../components/SectionInfo';
 import Steps from '../components/Steps';
+import FeaturesSection from '../sections/FeaturesSection';
 import HeroSection from '../sections/HeroSection';
 
 // ....
 
 export default function Home() {
-	const icons = [
-		FaFolder,
-		FaCodeBranch,
-		FaQrcode,
-		FaCode,
-		FaCube,
-		FaJsSquare,
-		FaClock,
-		FaSeedling,
-	];
-	const FeatureData = [
-		{
-			title: 'Organized Code',
-		},
-		{
-			title: 'Easy Folder Structure',
-		},
-		{
-			title: 'Code Splitting',
-		},
-		{
-			title: 'The Hassle-free Setup Process',
-		},
-		{
-			title: 'Code Splitting',
-		},
-		{
-			title: 'Organized Code',
-		},
-		{
-			title: 'Flat Design',
-		},
-		{
-			title: 'Intregrated Service',
-		},
-	];
 	return (
 		<>
 			<BootNav />
 			<HeroSection />
-			<SectionInfo
-				Heading='Know more about Design'
-				Description='hfghjfghjfghfhg'
-			/>
-			<div className='container'>
-				<div className='row'>
-					{FeatureData.map((item, idx) => {
-						const Icon = icons[idx];
-						return (
-							<>
-								<FeatureCard Title={item.title} icon={<Icon />} />
-							</>
-						);
-					})}
-				</div>
-			</div>
-
+			<FeaturesSection />
 			<Steps StepNo='1' StepTitle='Design' />
 			<div className='container'>
 				<div className='row'>
---
+++ sections/FeaturesSection.jsx
@@ -0,0 +1,74 @@
+import { Container, Row } from 'react-bootstrap';
+import {
+	FaClock,
+	FaCode,
+	FaCodeBranch,
+	FaCube,
+	FaFolder,
+	FaJsSquare,
+	FaQrcode,
+	FaSeedling,
+} from 'react-icons/fa';
+import FeatureCard from '../components/FeatureCard';
+import SectionInfo from '../components/SectionInfo';
+
+const FeaturesSection = () => {
+	const icons = [
+		FaFolder,
+		FaCodeBranch,
+		FaQrcode,
+		FaCode,
+		FaCube,
+		FaJsSquare,
+		FaClock,
+		FaSeedling,
+	];
+	const FeatureData = [
+		{
+			title: 'Organized Code',
+		},
+		{
+			title: 'Easy Folder Structure',
+		},
+		{
+			title: 'Code Splitting',
+		},
+		{
+			title: 'The Hassle-free Setup Process',
+		},
+		{
+			title: 'Code Splitting',
+		},
+		{
+			title: 'Organized Code',
+		},
+		{
+			title: 'Flat Design',
+		},
+		{
+			title: 'Intregrated Service',
+		},
+	];
+	return (
+		<>
+			<Container>
+				<SectionInfo
+					Heading='Know more about Design'
+					Description='lorem ipsum dolor sit amet aliquip ex ea commodo consequat.'
+				/>
+				<Row>
+					{FeatureData.map((item, idx) => {
+						const Icon = icons[idx];
+						return (
+							<>
+								<FeatureCard Title={item.title} icon={<Icon />} />
+							</>
+						);
+					})}
+				</Row>
+			</Container>
+		</>
+	);
+};
+
+export default FeaturesSection;
