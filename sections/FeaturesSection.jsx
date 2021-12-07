17c4150ee4aa130c7da4d9a5726dafb7a5dbe358
shafin <shafinshahriar340@gmail.com>
2021-12-08 00:48:35 +0600

    bootstrap css added to sections

--- sections/FeaturesSection.jsx
+++ sections/FeaturesSection.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container, Row } from 'react-bootstrap';
 import {
 	FaClock,
--- sections/HeroSection.jsx
+++ sections/HeroSection.jsx
@@ -1,3 +1,4 @@
+import 'bootstrap/dist/css/bootstrap.min.css';
 import { Container } from 'react-bootstrap';
 import styled from 'styled-components';
 import HeroHeading from '../components/HeroHeading';
